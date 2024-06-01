d4195cd3a598090c43e4b51949bebe2037cecbf1
Sergiu Luca <sergiuluca2001@gmail.com>
2024-06-02 00:40:14 +0300

    spawn 2 different players on same server

---
+++ scripts/SceneManager.cs
@@ -0,0 +1,31 @@
+using System;
+using Godot;
+
+public partial class SceneManager : Node2D
+{
+    [Export]
+    private PackedScene playerScene;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        int index = 0;
+        foreach (var player in GameManager.Players)
+        {
+            Player playerInstance = playerScene.Instantiate<Player>();
+            playerInstance.Name = player.Id.ToString();
+            AddChild(playerInstance);
+            foreach (Node2D spawnPoint in GetTree().GetNodesInGroup("PlayerSpawnPoints"))
+            {
+                if (int.Parse(spawnPoint.Name) == index)
+                {
+                    playerInstance.GlobalPosition = spawnPoint.GlobalPosition;
+                }
+            }
+            index++;
+        }
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
+}
