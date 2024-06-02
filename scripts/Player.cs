8f245c8b40b18a4806bf004b9c5f5d06b7d8e239
Sergiu Luca <sergiuluca2001@gmail.com>
2024-06-02 21:01:56 +0300

    add animation to player

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -6,6 +6,8 @@ public partial class Player : CharacterBody2D
     public const float Speed = 300.0f;
     public const float JumpVelocity = -400.0f;
 
+    private AnimatedSprite2D _animatedSprite;
+
     // Get the gravity from the project settings to be synced with RigidBody nodes.
     public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
@@ -13,6 +15,12 @@ public partial class Player : CharacterBody2D
     {
         GetNode<MultiplayerSynchronizer>("MultiplayerSynchronizer")
             .SetMultiplayerAuthority(int.Parse(Name));
+
+        // Get a reference to the AnimatedSprite2D node
+        _animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+        // Play the animation (replace "default" with the name of your animation)
+        _animatedSprite.Play("default");
     }
 
     public override void _PhysicsProcess(double delta)
